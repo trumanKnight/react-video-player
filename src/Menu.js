dda4ca2a59348589d20f59dc4200121289c1a10f
Truman Knight <cvilletruman@gmail.com>
2022-05-26 22:51:14 -0400

    Step 7

--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 export class Menu extends React.Component {
-  render() {
-    return (
-      <form onClick={this.props.chooseVideo}>
-        <input type="radio" name="src" value="fast" /> fast
-        <input type="radio" name="src" value="slow" /> slow
-        <input type="radio" name="src" value="cute" /> cute
-        <input type="radio" name="src" value="eek" /> eek
-      </form>
-    );
-  }
+    handleClick(e) {
+        const text = e.target.value;
+        this.props.chooseVideo(text);
+    }
+    render() {
+        return (
+            <form onClick={this.props.chooseVideo}>
+            <input type="radio" name="src" value="fast" /> fast
+            <input type="radio" name="src" value="slow" /> slow
+            <input type="radio" name="src" value="cute" /> cute
+            <input type="radio" name="src" value="eek" /> eek
+            </form>
+        );
+    }
 }
