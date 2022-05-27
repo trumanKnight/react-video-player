ead9592311903bcba9c91cd9f676e0ede3e0688c
Truman Knight <cvilletruman@gmail.com>
2022-05-26 22:52:01 -0400

    Step 8

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
     handleClick(e) {
         const text = e.target.value;
         this.props.chooseVideo(text);
